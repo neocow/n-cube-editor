4c3fec200de456bf45a32c386d4fe1d33861c8d5
wjwaters2 <wjwaters2@CVG-000218F-042.ga.afginc.com>
2014-07-11 14:38:06 -0400

    foo

--- src/main/java/com/cedarsoftware/controller/NCubeController.java
+++ src/main/java/com/cedarsoftware/controller/NCubeController.java
@@ -64,9 +64,10 @@ public class NCubeController extends BaseController implements INCubeController
         return isAllowed(app, version, "SNAPSHOT");
     }
 
+    //temp condition!!
     private static boolean isAllowed(String app, String version, String status)
     {
-        return "UD.REF.APP".equals(app) && "0.0.1".equals(version) && "SNAPSHOT".equals(status);
+        return "UD.REF.APP".equals(app) && "0.0.1".equals(version) && "SNAPSHOT".equals(status) || !"UD.REF.APP".equals(app);
     }
 
     public Object[] getCubeList(String filter, String app, String version, String status)
