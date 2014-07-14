6a2996420798b27f6c2b5c37c03e68cdf8e57583
schan2 <schan2@CVG-000218B-036.ga.afginc.com>
2014-07-14 12:57:33 -0400

    Add core-util for Cpr access, affected pom.xml.
    Apply taccount192 from executive dashboard for local development,
    affected SecurityManager.java and Cpr.

--- src/main/java/com/cedarsoftware/security/SecurityManager.java
+++ src/main/java/com/cedarsoftware/security/SecurityManager.java
@@ -1,9 +1,33 @@
 package com.cedarsoftware.security;
 
+import org.apache.log4j.Logger;
+
+import com.gaic.util.cpr.client.CPRPropertyProvider;
+
 public class SecurityManager implements Security
 {
+	private static final String LASTNAME = "LASTNAME";
+	private static final String FIRSTNAME = "FIRSTNAME";
+	private static final String HID = "HID";
+	private static final Logger _log = Logger.getLogger(SecurityManager.class);
+
     public String getUserId()
     {
         return "Jed";
     }
+    
+	public SecurityManager()
+	{
+		CPRPropertyProvider cprPropertyProvider = CPRPropertyProvider.getInstance();
+		String username;
+		try
+		{
+			username = cprPropertyProvider.getStringProperty("com.gaic.executivedashboard.securityframework.test.username");
+			_log.debug(String.format("ESAPI username [%s]", username));
+		}
+        catch(Exception e)
+        {
+			_log.error("Error occurred calling CPR",e);
+		}
+	}
 }
