44a6f043b5e2e3fb14da417ec958dff8d8db79d4
John DeRegnaucourt <john@cedarsoftware.com>
2015-06-21 14:09:47 -0400

    - removed console.log() statement - separated out code that built up URL

--- src/main/webapp/js/loadCubeList.js
+++ src/main/webapp/js/loadCubeList.js
@@ -3,9 +3,10 @@
  *
  *     This Javascript background thread is used to filter the list of cubes
  * based on the passed in filter string.  The data passed into onmessage(),
- * which comes from a postMessage(), is an array [] of two (2) elements.
- * [0] = NCE's cube list, which is a Map (object) 'cubeName' ==> infoDto
- * [1] = filter string.  This will be used to filter the above list.
+ * which comes from a postMessage(), is an array [] of three (3) elements:
+ * [0] = cube name filter list (cube names will be reduce to only those that 'contain' these characters, case-ins)
+ * [1] = content filter string (cubes will further be reduced to those that have JSON content that 'contain' these characters, case-ins)
+ * [2] = ApplicationID { 'app': 'foo', 'version':'1.0.0', 'status':'SNAPSHOT', 'branch':'joe'}
  *
  * @return a stable list of cubes, filtered by the passed in string.
  * @author John DeRegnaucourt
@@ -21,21 +22,7 @@ onmessage = function(e)
     var appIdString = JSON.stringify(appId);
     var req = new XMLHttpRequest();
 
-    var regexp = /\/([^\/]+)\//g;
-    var match = regexp.exec(location.pathname);
-    var url;
-    if (match == null || match.length != 2)
-    {
-        url = location.protocol + '//' + location.hostname + ":" + location.port + "/cmd/ncubeController/search";
-    }
-    else
-    {
-        var ctx = match[1];
-        url = location.protocol + '//' + location.hostname + ":" + location.port + "/" + ctx + "/cmd/ncubeController/search";
-    }
-
-    console.log(url);
-    req.open("GET", url + '?json=[' + appIdString + ',"' + filter + '","' + content + '"]', false);
+    req.open("GET", getSearchUrl() + '?json=[' + appIdString + ',"' + filter + '","' + content + '"]', false);
     req.send();
 
     if (req.response)
@@ -62,5 +49,24 @@ onmessage = function(e)
 
             postMessage(cubes);
         }
+    }
+
+    function getSearchUrl()
+    {
+        var regexp = /\/([^\/]+)\//g;
+        var match = regexp.exec(location.pathname);
+        var url;
+
+        if (match == null || match.length != 2)
+        {
+            url = location.protocol + '//' + location.hostname + ":" + location.port;
+        }
+        else
+        {
+            var ctx = match[1];
+            url = location.protocol + '//' + location.hostname + ":" + location.port + "/" + ctx;
+        }
+        url += '/cmd/ncubeController/search';
+        return url;
     }
 }
