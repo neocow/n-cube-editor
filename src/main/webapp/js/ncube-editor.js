060227249535425f7087b663adcf122b13a5663c
jdereg <jdereg@gmail.com>
2015-08-29 09:28:18 -0400

    - Working on tests / RPM tabs

--- src/main/webapp/js/ncube-editor.js
+++ src/main/webapp/js/ncube-editor.js
@@ -2,7 +2,21 @@
  * NCube Editor
  *     IDE for building and editing NCubes
  *
- * @author John DeRegnaucourt
+ * @author John DeRegnaucourt (jdereg@gmail.com)
+ *         <br>
+ *         Copyright (c) Cedar Software LLC
+ *         <br><br>
+ *         Licensed under the Apache License, Version 2.0 (the "License");
+ *         you may not use this file except in compliance with the License.
+ *         You may obtain a copy of the License at
+ *         <br><br>
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *         <br><br>
+ *         Unless required by applicable law or agreed to in writing, software
+ *         distributed under the License is distributed on an "AS IS" BASIS,
+ *         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *         See the License for the specific language governing permissions and
+ *         limitations under the License.
  */
 
 $(function ()
@@ -151,29 +165,7 @@ $(function ()
 
         addListeners();
 
-        var myLayout;
-        var secondaryLayout;
-
-        secondaryLayout = $('div#tests').layout({
-            name: "secondaryLayout"
-            ,   closable:					true	// pane can open & close
-            ,	resizable:					true	// when open, pane can be resized
-            ,	slidable:					true	// when closed, pane can 'slide' open over other panes - closes on mouse-out
-            ,	livePaneResizing:			true
-            ,	east__minSize:				170
-            ,   togglerLength_open:         60
-            ,   togglerLength_closed:       "100%"
-            ,	spacing_open:			5  // ALL panes
-            ,	spacing_closed:			5 // ALL panes
-            //            ,	south__spacing_open:			5  // ALL panes
-            //,	south__spacing_closed:			5 // ALL panes
-            ,  east__onresize: function()
-            {
-                calculateTestPanelSize();
-            }
-        });
-
-        myLayout = $('body').layout({
+        var myLayout = $('body').layout({
             name:   "BodyLayout"
             //	reference only - these options are NOT required because 'true' is the default
             ,   closable:					true	// pane can open & close
@@ -197,20 +189,14 @@ $(function ()
             //	enable state management
             ,	stateManagement__enabled:	false // automatic cookie load & save enabled by default
             //           ,	showDebugMessages:			true // log and/or display messages from debugging & testing code
-            //,  west__onresize:		"secondaryLayout.resizeAll" // resize ALL visible layouts nested inside
             ,  west__onresize: function()
             {
                 ncubeListPanel.height(west.height() - hApp - hStat - hVer - 158);
-                secondaryLayout.resizeAll();
+                //secondaryLayout.resizeAll();
             }
         });
 
         ncubeListPanel.height(west.height() - hApp - hStat - hVer - 158);
-        $(document).on( 'shown.bs.tab', 'a[data-toggle="tab"]', function (e)
-        {
-            secondaryLayout.resizeAll();
-            calculateTestPanelSize();
-        });
 
         $(document).keydown(function(e)
         {
@@ -521,13 +507,6 @@ $(function ()
     function buildNCubeEditMenu()
     {
         // TODO: Add when we can invoke cut, copy, and paste from the drop down
-    }
-
-    function calculateTestPanelSize()
-    {
-        var east = $('#testLayoutEast');
-        var testList = $('#testList').find('> .panel-body');
-        testList.height(east.height() - 47);
     }
 
     function clearSearch()
---
+++ src/main/webapp/js/test.js
@@ -0,0 +1,85 @@
+/**
+ * Test Panel for N-Cube Editor
+ *
+ * @author Ken Partlow (kpartlow@gmail.com)
+ *         <br>
+ *         Copyright (c) Cedar Software LLC
+ *         <br><br>
+ *         Licensed under the Apache License, Version 2.0 (the "License");
+ *         you may not use this file except in compliance with the License.
+ *         You may obtain a copy of the License at
+ *         <br><br>
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *         <br><br>
+ *         Unless required by applicable law or agreed to in writing, software
+ *         distributed under the License is distributed on an "AS IS" BASIS,
+ *         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *         See the License for the specific language governing permissions and
+ *         limitations under the License.
+ */
+
+$(function ()
+{
+    $.info = {};
+
+    $.loadData = function()
+    {
+        if (!nce().getCubeMap() || !nce().doesCubeExist())
+        {
+            return;
+        }
+
+        var info = nce().getCubeMap()[(nce().getSelectedCubeName() + '').toLowerCase()];
+        if (!info)
+        {
+            return;
+        }
+
+        var secondaryLayout = $('div#testTab').layout({
+            name: "secondaryLayout"
+            ,   closable:					true	// pane can open & close
+            ,	resizable:					true	// when open, pane can be resized
+            ,	slidable:					true	// when closed, pane can 'slide' open over other panes - closes on mouse-out
+            ,	livePaneResizing:			true
+            ,	east__minSize:				170
+            ,   togglerLength_open:         60
+            ,   togglerLength_closed:       "100%"
+            ,	spacing_open:			5  // ALL panes
+            ,	spacing_closed:			5 // ALL panes
+            //            ,	south__spacing_open:			5  // ALL panes
+            //,	south__spacing_closed:			5 // ALL panes
+            ,  east__onresize: function()
+            {
+                calculateTestPanelSize();
+            }
+        });
+
+        //secondaryLayout.resizeAll();
+        calculateTestPanelSize();
+    };
+
+    function calculateTestPanelSize()
+    {
+        var east = $('#testLayoutEast');
+        var testList = $('#testList').find('> .panel-body');
+        testList.height(east.height() - 47);
+    }
+});
+
+function nce()
+{
+    return $.info.fn;
+}
+
+function tabActivated(info)
+{
+    try
+    {
+        $.info = info;
+        $.loadData();
+    }
+    catch (e)
+    {
+        console.log(e);
+    }
+}
