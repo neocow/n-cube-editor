2ddbb6c9e9d8a58ff36ce46b8252f6342b7a454f
tpollack <tpollack@gaig.com>
2015-12-09 11:58:39 -0500

    [RA-22320] Start set up to use handsontable for ncube tab.

--- src/main/webapp/js/ntwobe.js
+++ src/main/webapp/js/ntwobe.js
@@ -1,31 +1,14 @@
-/**
- * Main NCube editor
- *
- * @author John DeRegnaucourt (jdereg@gmail.com)
- *         <br>
- *         Copyright (c) Cedar Software LLC
- *         <br><br>
- *         Licensed under the Apache License, Version 2.0 (the "License");
- *         you may not use this file except in compliance with the License.
- *         You may obtain a copy of the License at
- *         <br><br>
- *         http://www.apache.org/licenses/LICENSE-2.0
- *         <br><br>
- *         Unless required by applicable law or agreed to in writing, software
- *         distributed under the License is distributed on an "AS IS" BASIS,
- *         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *         See the License for the specific language governing permissions and
- *         limitations under the License.
- */
-
 var NCubeEditor2 = (function ($)
 {
+    var headerAxisNames = ['trait','traits','businessDivisionCode','bu','month','months','col','column','cols','columns'];
     var nce = null;
     var hot = null;
-    var fieldNames = null;
-    var traits = null;
+    var cols = null;
     var rowNums = null;
     var data = null;
+    var cubeName = null;
+    var axes = null;
+    var mergedCellInfo = null;
 
     var init = function(info) {
         if (!nce) {
@@ -62,94 +45,235 @@ var NCubeEditor2 = (function ($)
 
         handleCubeData(JSON.parse(result.data));
 
-        hot = new Handsontable(document.getElementById('ncube-content'), getHotSettings());
+        hot = new Handsontable(document.getElementById('hot-container'), getHotSettings());
         hot.render();
     };
 
     var handleCubeData = function(cubeData) {
-        var handleCellData = function(cells) {
-            for (var i = 0; i < cells.length; i++) {
-                var cell = cells[i];
-                var cellLocationInfo = getLogicalNamesFromCoordinates(cell);
 
-                var dataRow = findRowFromHeaders(cellLocationInfo.row);
-                if (!dataRow) {
-                    dataRow = initRow(cellLocationInfo.row);
-                    data.push(dataRow);
+        var determineAxesOrder = function(axes) {
+            var horizontal;
+            axes.sort(function(a, b) {
+                return b.columns.length - a.columns.length;
+            });
+
+            var delta;
+            var smallestDelta = Number.MAX_VALUE;
+            for (var i = 0; i < axes.length; i++) {
+                var axis = axes[i];
+                if (headerAxisNames.indexOf(axis.name) > -1) {
+                    horizontal = i;
+                    break;
                 }
-
-                dataRow[cellLocationInfo.trait] = cell.value;
-            }
-        };
-
-        var findRowFromHeaders = function(rowHeader) {
-            for (var r = 0; r < data.length; r++) {
-                if (data[r].rowId === rowHeader) {
-                    return data[r];
-                }
-            }
-            return null;
-        };
-
-        var getLogicalNamesFromCoordinates = function(cell) {
-            var rowHeader;
-            var traitHeader;
-            for (var i = 0; i < cell.id.length; i++) {
-                var curId = cell.id[i];
-                if (traitMap.hasOwnProperty(curId)) {
-                    traitHeader = traitMap[curId];
-                } else if (rowMap.hasOwnProperty(curId)) {
-                    rowHeader = rowMap[curId];
+                delta = Math.abs(axis.columns.length - 12);
+                if (delta < smallestDelta) {
+                    smallestDelta = delta;
+                    horizontal = i;
                 }
             }
 
-            return {row:rowHeader, trait:traitHeader};
+            horizontal = axes.splice(horizontal, 1);
+            axes.push(horizontal[0]);
+            return axes;
         };
 
-        // creates a new row with blank columns
-        var initRow = function(rowId) {
-            var row = {rowId:rowId};
-            for (var i = 0; i < traits.length; i++) {
-                row[traits[i]] = '';
-            }
-            return row;
-        };
+        var setUpDataTable = function() {
+            cubeName = cubeData.ncube;
+            data = [];
+            rowNums = ['',''];
+            cols = [''];
+            mergedCellInfo = [];
 
-        var cubeName = cubeData.ncube;
-        var axes = cubeData.axes;
-        var numAxes = axes.length - 1;
+            data[0] = [cubeName];
+            var totalRows = 1;
+            var rowOffset = 2;
+            var numAxes = axes.length;
 
-        traits = [];
-        fieldNames = [];
-        rowNums = [];
-        data = [];
+            if (numAxes > 1) {
+                var horizAxis = axes[numAxes - 1];
 
-        var traitMap = {};
-        var rowMap = {};
-        var row = 0;
+                if (numAxes > 2) {
+                    for (var i = 0; i < numAxes - 2; i++) {
+                        data[0].push('');
+                        cols.push('');
+                    }
+                }
+                data[0].push(horizAxis.name);
+                var columns = horizAxis.columns;
+                for (var columnIndex = 0; columnIndex < columns.length; columnIndex++) {
+                    var col = columns[columnIndex];
+                    cols.push({coords: [col.id], value: col.value});
+                    data[0].push('');
+                }
+                if (horizAxis.hasDefault) {
+                    cols.push({coords: [horizAxis.name + '_default'], value: 'Default'});
+                } else {
+                    data[0].splice(data[0].length - 1);
+                }
 
-        for (var i = 0; i < axes.length; i++) {
-            var axis = axes[i];
-            if (axis.name.toLowerCase() === 'trait') {
-                var traitCols = axis.columns;
-                for (var x = 0; x < traitCols.length; x++) {
-                    var trait = traitCols[x];
-                    traits.push(trait.value);
-                    traitMap[trait.id] = trait.value;
+                for (var axisNum = 0; axisNum < numAxes - 1; axisNum++) {
+                    var axis = axes[axisNum];
+                    var numCols = axis.columns.length;
+                    if (axis.hasDefault) {
+                        numCols++;
+                    }
+                    totalRows *= numCols;
+                }
+
+                data[1] = JSON.parse(JSON.stringify(cols));
+                var newBlankRow = [];
+                for (var col = 0; col < cols.length; col++) {
+                    newBlankRow.push({coords: [], value: ''});
+                }
+
+                for (var rowNum = 1; rowNum <= totalRows; rowNum++) {
+                    rowNums.push(rowNum.toString());
+                    data.push(JSON.parse(JSON.stringify(newBlankRow)));
+                }
+
+                var remainingAxesColumnCount = totalRows;
+                for (var axisNum = 0; axisNum < numAxes - 1; axisNum++) {
+                    var axis = axes[axisNum];
+                    data[1][axisNum] = axis.name;
+                    var axisColAmt = axis.columns.length;
+                    if (axis.hasDefault) {
+                        axisColAmt++;
+                    }
+                    var curColNum = -1;
+                    remainingAxesColumnCount = remainingAxesColumnCount / axisColAmt;
+                    var currentMergeStart = rowOffset;
+                    var currentMergeSpan = 1;
+
+                    for (var rowNum = 0; rowNum < totalRows; rowNum++) {
+                        var cell = {coords: [], value: ''};
+
+                        var isNewColumnIteration = rowNum % remainingAxesColumnCount === 0;
+                        if (isNewColumnIteration) {
+                            curColNum++;
+                            if (currentMergeSpan > 1) {
+                                mergedCellInfo.push({
+                                    row: currentMergeStart,
+                                    col: axisNum,
+                                    rowspan: currentMergeSpan,
+                                    colspan: 1
+                                });
+                                currentMergeSpan = 1;
+                                currentMergeStart = rowNum + rowOffset;
+                            }
+
+                            if (curColNum >= axisColAmt) {
+                                curColNum = 0;
+                            }
+                        } else {
+                            currentMergeSpan++;
+                        }
+
+                        if (axisNum > 0) {
+                            cell.coords = JSON.parse(JSON.stringify(data[rowNum + rowOffset][axisNum - 1].coords));
+                        }
+                        if (curColNum < axis.columns.length) {
+                            var curCol = axis.columns[curColNum];
+                            cell.coords.push(curCol.id);
+                            if (isNewColumnIteration) {
+                                cell.value = curCol.value;
+                            }
+                        } else {
+                            cell.coords.push(axis.name + '_default');
+                            cell.value = 'Default';
+                        }
+                        data[rowNum + rowOffset][axisNum] = cell;
+                    }
+                    if (currentMergeSpan > 1) {
+                        mergedCellInfo.push({
+                            row: currentMergeStart,
+                            col: axisNum,
+                            rowspan: currentMergeSpan,
+                            colspan: 1
+                        });
+                    }
+                }
+
+                for (var y = 2; y < data.length; y++) {
+                    var rowCoords = data[y][numAxes - 2].coords;
+                    for (var x = numAxes - 1, len = data[0].length; x < len; x++) {
+                        var newCoords = JSON.parse(JSON.stringify(rowCoords));
+                        var colCoords = data[1][x].coords;
+                        newCoords.push(colCoords[0]);
+                        data[y][x].coords = newCoords;
+                    }
                 }
             } else {
-                // TODO - change this for n-dimensional cubes
-                traits.unshift(''); // blank for row header(s)
+                data[0].push('');
+                var axis = axes[0];
                 var columns = axis.columns;
-                for (var x = 0; x < columns.length; x++) {
-                    var column = columns[x];
-                    fieldNames.push(column.value);
-                    rowNums.push(++row);
-                    rowMap[column.id] = column.value;
+                data[1] = [axis.name, ''];
+
+                for (var columnNum = 0; columnNum < columns.length; columnNum++) {
+                    var dataRowNum = columnNum + rowOffset;
+                    var column = columns[columnNum];
+                    rowNums.push(dataRowNum.toString());
+                    data[dataRowNum] = [{coords:[column.id], value:column.value},{coords:[column.id], value:''}];
+                }
+                if (axis.hasDefault) {
+                    var defaultId = axis.name + '_default';
+                    data.push([{coords:[defaultId], value:'Default'},{coords:[defaultId], value:''}]);
                 }
             }
-        }
+        };
 
+        var handleCellData = function(cells) {
+            var findDataCellFromCoords = function(coords) {
+                if (axes.length > 1) {
+                    var findColumnFromCoords = function () {
+                        var headerRow = data[1];
+                        for (var x = axes.length - 1; x < headerRow.length; x++) {
+                            var colCoord = headerRow[x].coords[0];
+                            if (coords.indexOf(colCoord) > -1 || colCoord.toString().indexOf('_default') > -1) {
+                                return x;
+                            }
+                        }
+                    };
+
+                    var findRowFromCoords = function () {
+                        var headerColNum = axes.length - 2;
+                        for (var y = 2; y < data.length; y++) {
+                            var rowCoords = data[y][headerColNum].coords;
+                            var found = true;
+                            for (var i = 0; i < rowCoords.length; i++) {
+                                var rowCoord = rowCoords[i];
+                                if (coords.indexOf(rowCoord) < 0 && rowCoord.toString().indexOf('_default') < 0) {
+                                    found = false;
+                                    break;
+                                }
+                            }
+                            if (found) {
+                                return y;
+                            }
+                        }
+                    };
+
+                    var columnNum = findColumnFromCoords();
+                    var rowNum = findRowFromCoords();
+                    return data[rowNum][columnNum];
+                } else {
+                    for (var rowNum = 2; rowNum < data.length; rowNum++) {
+                        var coord = data[rowNum][1].coords[0];
+                        if (coords.indexOf(coord) > -1 || coord.toString().indexOf('_default') > -1) {
+                            return data[rowNum][1];
+                        }
+                    }
+                }
+            };
+
+            for (var i = 0; i < cells.length; i++) {
+                var cell = cells[i];
+                var dataCell = findDataCellFromCoords(cell.id);
+                dataCell.value = cell.value;
+            }
+        };
+
+        axes = determineAxesOrder(cubeData.axes);
+        setUpDataTable();
         handleCellData(cubeData.cells);
     };
 
@@ -160,38 +284,74 @@ var NCubeEditor2 = (function ($)
             enterBeginsEditing: false,
             enterMoves: {row: 1, col: 0},
             tabMoves : {row: 0, col: 1},
-            //colHeaders: traitTable.names,
-            colHeaders: traits,
-            //rowHeaders: rowNums,
-            //startCols: traitTable.names.length,
-            startCols: traits.length,
+            rowHeaders: rowNums,
+            startCols: cols.length,
             startRows: rowNums.length,
-            contextMenu: true,
+            contextMenu: false,
             manualColumnResize: true,
             manualRowResize: true,
-            fixedColumnsLeft: 1,
-            height:500,
+            fixedColumnsLeft: axes.length - 1,
+            fixedRowsTop: 2,
+            currentRowClassName: 'handsonCurrentRow',
+            currentColClassName: 'handsonCurrentCol',
+            outsideClickDeselects: false,
+            mergeCells: mergedCellInfo,
+            autoColumnSize: true,
             cells: function (row, col, prop) {
                 return {renderer:categoryRenderer};
+            }
+            ,
+            afterSelectionEnd: function(r, c, r2, c2) {
+                console.log(hot.getDataAtCell(r, c));
             }
         };
     };
 
-    var categoryRenderer = function(instance, td, row, col, prop, value, cellProperties)
-    {
+    var categoryRenderer = function(instance, td, row, col, prop, value, cellProperties) {
         Handsontable.renderers.TextRenderer.apply(this, arguments);
-        if (col == 0) {
-            td.innerHTML = fieldNames[row];
+
+        // cube name
+        if (row === 0 && (col < axes.length - 1 || col === 0)) {
+            td.style.background = '#6495ED';
+            td.style.color = 'white';
+            td.style.opacity = '1';
+            cellProperties.readOnly = true;
+            if (col < axes.length - 2) {
+                td.style.borderRight = 'none';
+            }
         }
 
-        //$.each(traitTable, function(item)
-        //{
-        //    if (item.startsWith('css:') && traitTable[item][col])
-        //    {
-        //        var style = item.replace(/^css:/, '');
-        //        td.style[style] = traitTable[item][col];
-        //    }
-        //});
+        // horizontal axis metadata area
+        else if (row === 0 || (row === 1 && (col < axes.length - 1 || col === 0))) {
+            td.style.background = '#4D4D4D';
+            td.style.color = 'white';
+            td.style.opacity = '1';
+            cellProperties.readOnly = true;
+
+            // rest of the top row
+            if (row === 0 && col > 0) {
+                td.style.borderRight = 'none';
+            }
+        }
+
+        // axis headers
+        else if (row === 1 || col < axes.length - 1) {
+            td.style.background = '#929292';
+            td.style.color = 'white';
+            if (col < axes.length - 1) {
+                td.className = "htMiddle";
+            }
+        }
+
+        // otherwise odd rows
+        else if (row % 2 != 0) {
+            td.style.background = '#e0e0e0';
+        }
+
+        // to maintain meta data, cell data is passed as an object, with the displayable data stored as value
+        if (typeof value === 'object') {
+            td.innerHTML = value.value;
+        }
     };
 
     // =============================================== End Cell Editing ================================================
