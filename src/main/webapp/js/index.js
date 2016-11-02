e52767c691f5a433ee2b2899d3b282992619e66c
John DeRegnaucourt <jdereg@gmail.com>
2016-11-02 19:17:49 -0400

    - Removed single CUBE update from menu

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -669,7 +669,7 @@ var NCE = (function ($) {
         if (cubeInfo[CUBE_INFO.BRANCH] !== head) {
             html += '<li><a href="#" class="anc-commit-cube">Commit...</a></li>'
                   + '<li><a href="#" class="anc-rollback-cube">Rollback...</a></li>'
-                  + '<li><a href="#" class="anc-update-cube">Update from HEAD</a></li>'
+                  // + '<li><a href="#" class="anc-update-cube">Update from HEAD</a></li>'
                   + '<div class="divider"/>'
                   + '<li><a href="#" class="anc-delete-cube">Delete...</a></li>'
                   + '<li><a href="#" class="anc-duplicate-cube">Duplicate...</a></li>'
